a23854a4282587c077b52880a929b0c9e384a0fb
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2019-07-23 21:10:18 -0600

    React Components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import ReactDom from 'react-dom';
 
 
+const Header = ({message}) => {
+    return(
+        <h2 className="text-center">
+            {/* Hello React Components!! */}
+            {message}
+         </h2>
+    );
+};
+
+Header.propTypes = {
+    message: PropTypes.string
+};
+
+const App = () => {
+    return (
+        <div className="App">
+            <Header message="Naming Contests"/>
+            <div>
+                ...
+            </div>
+        </div>
+    );
+};
 
 ReactDom.render(
     //React.createElement('h2', null, 'Hello React'),
-    <h2 className="text-center">
-        Hello React with JSX!!
-    </h2>,
+    <App />,
     document.getElementById('root')
 );
