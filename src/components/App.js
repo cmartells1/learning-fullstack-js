1a01fcf3ff6351fe8537070724f5e25bccb28991
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2019-08-20 18:05:25 -0600

    update to work with mongoDB

--- src/components/App.js
+++ src/components/App.js
@@ -81,6 +81,16 @@ class App extends React.Component {
         //lookup contests
         //this.state.contests[contestId] this gets the id from the contest object
     };
+
+    fetchNames = (nameIds) => {
+        if(nameIds.length === 0){
+            return;
+        }
+        api.fetchNames(nameIds).then(names => {
+            this.setState({names});
+        })
+    };
+
     currentContest (){
         return this.state.contests[this.state.currentContestId];
     }
@@ -91,11 +101,22 @@ class App extends React.Component {
         }
         return 'Naming Contests';
     }
+
+    lookupName = (nameId) => {
+        if(!this.state.names || !this.state.names[nameId]){
+            return {
+                name: '...'
+            };
+        }
+        return this.state.names[nameId];
+    };
     currentContent() {
         if(this.state.currentContestId) 
         {
             return <Contest
-                    contestListClick={this.fetchContestList} 
+                    contestListClick={this.fetchContestList}
+                    fetchNames ={this.fetchNames}
+                    lookupName = {this.lookupName}
                     {...this.currentContest()}/>;
         }
 
