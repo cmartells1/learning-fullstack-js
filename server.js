d49aa331e6b3b0277f279964288c2e95445df161
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2019-08-27 20:12:49 -0600

    Api set up id from db

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ import express from 'express';
 import apiRouter from './api';
 import sassMiddleware from 'node-sass-middleware';
 import path from 'path';
+import bodyParser from 'body-parser';
 
 const server = express();
+server.use(bodyParser.json());
 
 server.use(sassMiddleware({
     src:path.join(__dirname, 'sass'),
@@ -26,7 +28,10 @@ server.get(['/', '/contest/:contestId'], (req,res) => {
             initialMarkup,
             initialData
         });
-    }).catch(console.error)
+    }).catch(error => {
+        console.error(error)
+        res.status(404).send('Bad Request');
+    })
 });
 
 server.use(express.static('public'));
