d99129b5f26a6625f262861b2f944f68c3a3e881
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2019-08-30 17:29:33 -0600

    finished app

--- api/index.js
+++ api/index.js
@@ -1,61 +1,94 @@
-import express from 'express';
-import { MongoClient, ObjectID } from 'mongodb';
-import assert from 'assert';
-import config from '../config';
-
-
+import express from "express";
+import { MongoClient, ObjectID } from "mongodb";
+import assert from "assert";
+import config from "../config";
 
 let mdb;
-MongoClient.connect(config.mongodbUri, (err, client) =>{
-    assert.equal(null, err);
+MongoClient.connect(config.mongodbUri, (err, client) => {
+  assert.equal(null, err);
 
-    mdb = client.db('test');
+  mdb = client.db("test");
 });
 
 const router = express.Router();
 
-router.get('/contests', (req, res) =>{
-    let contests = {}; 
-    mdb.collection('contests').find({})
-        .project({         
-            categoryName:1,
-            contestName: 1
-        })
-        .each((err, contest) =>{
-            assert.equal(null,err);            
+router.get("/contests", (req, res) => {
+  let contests = {};
+  mdb
+    .collection("contests")
+    .find({})
+    .project({
+      categoryName: 1,
+      contestName: 1
+    })
+    .each((err, contest) => {
+      assert.equal(null, err);
 
-            if(!contest){// no more contests
-                res.send({contests});
-                return;
-            }
-            contests[contest._id] = contest;
-        });
+      if (!contest) {
+        // no more contests
+        res.send({ contests });
+        return;
+      }
+      contests[contest._id] = contest;
+    });
 });
 
-router.get('/names/:nameIds', (req, res) =>{
-    const nameIds = req.params.nameIds.split(',').map(ObjectID);
-    let names = {}; 
-    mdb.collection('names').find({_id: {$in: nameIds}})        
-        .each((err, name) =>{
-            assert.equal(null,err);            
+router.get("/names/:nameIds", (req, res) => {
+  const nameIds = req.params.nameIds.split(",").map(ObjectID);
+  let names = {};
+  mdb
+    .collection("names")
+    .find({ _id: { $in: nameIds } })
+    .each((err, name) => {
+      assert.equal(null, err);
 
-            if(!name){// no more names
-                res.send({names});
-                return;
-            }
-            names[name._id] = name;
-        });
+      if (!name) {
+        // no more names
+        res.send({ names });
+        return;
+      }
+      names[name._id] = name;
+    });
 });
 
-router.get('/contests/:contestId', (req, res) =>{
-        mdb.collection('contests').findOne({_id: ObjectID(req.params.contestId) })
-        .then(contest => res.send(contest))
-        .catch(console.error);
-    
+router.get("/contests/:contestId", (req, res) => {
+  mdb
+    .collection("contests")
+    .findOne({ _id: ObjectID(req.params.contestId) })
+    .then(contest => res.send(contest))
+    .catch(error => {
+        console.error(error)
+        res.status(404).send('Bad Request');
+    });
 });
 
-router.post('/names', (req, res) => {
-    res.send(req.body);
+router.post("/names", (req, res) => {
+  const contestId = ObjectID(req.body.contestId);
+  const name = req.body.newName;
+
+  mdb
+    .collection("names")
+    .insertOne({ name })
+    .then(result =>
+      mdb
+        .collection("contests")
+        .findAndModify(
+          { _id: contestId }, //what your searching by
+          [], //how to sort
+          { $push: { nameIds: result.insertedId } }, //what you want to modify
+          { new: true }
+        )
+        .then(doc =>
+          res.send({
+            updatedContest: doc.value,
+            newName: { _id: result.insertedId, name }
+          })
+        )
+    )
+    .catch(error => {
+        console.error(error)
+        res.status(404).send('Bad Request');
+    });
 });
 
 export default router;
--- src/components/Contest.js
+++ src/components/Contest.js
@@ -5,6 +5,12 @@ class Contest extends Component {
   componentDidMount(){
     this.props.fetchNames(this.props.nameIds);
   }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.addName(this.refs.newNameInput.value, this.props._id);
+    this.refs.newNameInput.value = '';
+  };
+
   render() {
     return (
       <div className="Contest">
@@ -35,16 +41,17 @@ class Contest extends Component {
             <h3 className="panel-title">Propose a New Name</h3>
           </div>
           <div className="panel-body">
-            <form>
+            <form onSubmit={this.handleSubmit}>
               <div className="input-group">
                 <input
                   type="text"
                   placeholder="New Name Here..."
+                  ref="newNameInput"
                   className="form-control"
                 />
                 <span className="input-group-btn">
                   <button type="submit" className="btn btn-info">
-                    Sumbit
+                    Submit
                   </button>
                 </span>
               </div>
@@ -61,11 +68,13 @@ class Contest extends Component {
 }
 
 Contest.propTypes = {
+  _id: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   contestListClick: PropTypes.func.isRequired,
   fetchNames: PropTypes.func.isRequired,
   nameIds: PropTypes.array.isRequired,
-  lookupName: PropTypes.func.isRequired
+  lookupName: PropTypes.func.isRequired,
+  addName: PropTypes.func.isRequired
 };
 
 export default Contest;
